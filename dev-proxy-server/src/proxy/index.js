90093f82ca615c368c25d7cd86355c1f21162a7b
Anson Tsao <tsaoa@acm.org>
2019-02-26 22:07:53 -0800

    Added echoHandler

--- dev-proxy-server/src/proxy/index.js
+++ dev-proxy-server/src/proxy/index.js
@@ -8,6 +8,7 @@ import _ from 'lodash'
 import async from 'async'
 import { certificateFor } from 'devcert'
 import fs from 'fs'
+// import util from 'util'
 
 function proxyHandler (target) {
   const redir = proxy({
@@ -21,27 +22,49 @@ function proxyHandler (target) {
   }
 }
 
-async function setupProxy ({ http, https, host, options, creds }) {
-  if (options.https) {
-    let cert
-    if (options.certPath) {
-      cert = {
-        cert: fs.readFileSync(options.certPath),
-        key: fs.readFileSync(options.keyPath)
-      }
-    } else {
-      cert = await certificateFor(host)
+function echoHandler () {
+  return (req, res, next) => {
+    // res.end(util.inspect(req))
+    const out = {
+      originalUrl: req.originalUrl,
+      url: req.url,
+      hostname: req.hostname,
+      method: req.method,
+      headers: req.headers,
+      body: []
     }
-    creds.push({
-      hostname: host,
-      cert: cert.cert,
-      key: cert.key
-    })
+    req
+      .on('data', (chunk) => {
+        out.body.push(chunk)
+      })
+      .on('end', () => {
+        out.body = Buffer.concat(out.body).toString()
+        if (req.headers['content-type'] === 'application/json') {
+          out.body = JSON.parse(out.body)
+        }
+        res.setHeader('Content-Type', 'application/json')
+        res.end(JSON.stringify(out, null, 2))
+      })
+  }
+}
+
+async function setupProxy ({ http, https, host, options }) {
+  if (options.https) {
     console.log(`https://${host} -> ${options.target}`)
     https.use(vhost(host, proxyHandler(options.target)))
   } else {
     console.log(`http://${host} -> ${options.target}`)
     http.use(vhost(host, proxyHandler(options.target)))
+  }
+}
+
+async function setupEcho ({ http, https, host, options }) {
+  if (options.https) {
+    console.log(`echo https://${host}`)
+    https.use(vhost(host, echoHandler()))
+  } else {
+    console.log(`echo http://${host}`)
+    http.use(vhost(host, echoHandler()))
   }
 }
 
@@ -53,10 +76,29 @@ function setupVhosts (config, proxyOptions = {}) {
     const creds = []
 
     async.eachSeries(_.toPairs(config), async ([host, options], cb) => {
+      if (options.https) {
+        let cert
+        if (options.certPath) {
+          cert = {
+            cert: fs.readFileSync(options.certPath),
+            key: fs.readFileSync(options.keyPath)
+          }
+        } else {
+          cert = await certificateFor(host)
+        }
+        creds.push({
+          hostname: host,
+          cert: cert.cert,
+          key: cert.key
+        })
+      }
+
       if (options.type === 'proxy') {
-        await setupProxy({ http, https, host, options, creds })
+        await setupProxy({ http, https, host, options })
         cb()
       } else if (options.type === 'echo') {
+        await setupEcho({ http, https, host, options })
+        cb()
       }
     }, (err) => {
       if (err) reject(err)
